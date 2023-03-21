986ac5a42c318efe5da636c1a2af99f8069bea89
Dino <dino15031992@gmail.com>
2023-03-21 16:25:56 +0100

    add Javadoc commit

--- src/main/java/it/develhope/Email01/Email01Application.java
+++ src/main/java/it/develhope/Email01/Email01Application.java
@@ -3,9 +3,17 @@ package it.develhope.Email01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Drumstyle92
+ * Class that initializes Spring Boot
+ */
 @SpringBootApplication
 public class Email01Application {
 
+	/**
+	 * @param args Main parameter
+	 * Main method to be able to start the application
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Email01Application.class, args);
 	}
